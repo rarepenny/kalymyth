8c6592c7867639eef48346bee5bb4ea5d2c96c81
rarepenny <33160365+rarepenny@users.noreply.github.com>
2017-10-30 14:58:28 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -3,8 +3,38 @@ class Parser {
     this.grammar = this.compile(grammar);
   }
   compile (grammar) {
-    return grammar
+    return grammar;
   }
-  tokenize (sentence) {
-  
+  tokenize (phrase) {
+    return phrase;
+  }
+}
+
+class World {
+  constructor (title) {
+    this.title = title;
+    document.getElementById("title").textContent = this.title;
+    this.nodes = [];
+  }
+  addNode (node) {
+    this.nodes.append(node);
+  }
+}
+
+class Node {
+  constructor (name, description, contents, ...properties) {
+    this.name = name;
+    this.description = description;
+    this.contents = contents;
+    this.property = {};
+    for prop in properties.keys() {
+      this.property[prop] = properties[prop];
+    }
+  }
+  describe () {
+    var outputParagraph = document.createElement('p');
+    var outputContent = document.createTextNode(this.description);
+    outputParagraph.appendChild(outputContent);
+    document.getElementById("paragraphs").appendChild(outputParagraph);
+  }
 }
