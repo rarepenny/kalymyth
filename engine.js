a675b9fcf8de8e4140d51754ba72f2ff1745f1fd
rarepenny <33160365+rarepenny@users.noreply.github.com>
2017-11-01 11:43:48 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -22,18 +22,46 @@ class World {
 }
 
 class Node {
-  constructor (name, description, contents, ...properties) {
-    this.name = name;
-    this.description = description;
-    this.contents = contents;
-    this.property = {};
+  constructor (name, description="Nothing of interest.", contents=[], ...properties) {
+    this.property = {
+      "name": name,
+      "description": description,
+      "light": 0,
+      "contents": (contents || []),
+      "takeable": true,
+      "droppable": true,
+      "weight": -1,
+      "volume": -1
+    };
     for prop in properties.keys() {
       this.property[prop] = properties[prop];
     }
   }
-  describe () {
-    var outputParagraph = document.createElement('p');
-    var outputContent = document.createTextNode(this.description);
+  expectProperty (key, value) {
+    if (this.property[key] === value) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  safeSetProperty (key, value) {
+    if !(key in this.property) {
+      this.property[key] = value;
+    }
+  }
+  getProperty (key) {
+    return this.property[key];
+  }
+  setProperty (key, value) {
+    this.property[key] = value;
+  }
+  describe (override) {
+    var outputParagraph = document.createElement("p");
+    if (override) {
+      var outputContent = document.createTextNode(override);
+    } else {
+      var outputContent = document.createTextNode(this.getProperty("description"));
+    }
     outputParagraph.appendChild(outputContent);
     document.getElementById("paragraphs").appendChild(outputParagraph);
   }
