86cbd14133908eaf51c6217cca2dcb1b1b989b9e
rarepenny <33160365+rarepenny@users.noreply.github.com>
2017-11-03 21:48:49 -0400

    Update plotkey.core.js

--- src/plotkey.core.js
+++ src/plotkey.core.js
@@ -1,21 +1,29 @@
-var plotTools = {
-	writeParagraph: function (text, cause="?") {
-		var paragraph = $("<p data-cause=\"" + cause + "\"></p>");
-		// Data-cause allows the player to view the command which caused the output of this paragraph by mousing over it.
-		paragraph.text(text);
-		$("#paragraphs").append(paragraph);
-	},
-	grabInput: function () {
-  		var inputToReturn = $("#gameCommandInput").val();
-    		$("#gameCommandInput").val('');
-    		return inputToReturn;
+function writeParagraph (text, cause="?") {
+	var paragraph = $("<p data-cause=\"" + cause + "\"></p>");
+	// Data-cause allows the player to view the command which caused the output of this paragraph by mousing over it.
+	paragraph.text(text);
+	$("#paragraphs").append(paragraph);
+}
+
+function grabInput () {
+  	var inputToReturn = $("#gameCommandInput").val();
+    	$("#gameCommandInput").val('');
+	return inputToReturn;
+}
+
+function parse (userInput) {
+	userInput = userInput.split(/\s+/, 1);
+	if (userInput[0] in ['take', 'drop', 'go', 'see']) {
+		writeParagraph('valid input', userInput[0]);
+	} else {
+		writeParagraph('huh?', userInput[0]);
 	}
 }
 
 $(document).ready(function () {
 	$("#gameCommandInput").on('keyup', function(e) {
 		if (e.keyCode === 13) {
-			plotTools.writeParagraph(plotTools.grabInput());
+			parse(grabInput());
 		}
 	});
 });
