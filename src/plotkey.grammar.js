0c854ebfa8f297a238baede9b38f686bd2493cbe
rarepenny <33160365+rarepenny@users.noreply.github.com>
2017-11-03 21:17:01 -0400

    Update plotkey.grammar.js

--- src/plotkey.grammar.js
+++ src/plotkey.grammar.js
@@ -1,11 +1,1 @@
-/* This javascript file will parse EBNF formal grammars and return tokens. */
 
-var currentGrammar = {};
-
-function setProductionRules (rules) {
-  rules = rules.split(';\n');
-}
-
-function parseEBNF (phrase) {
-  
-}
