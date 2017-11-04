cacd9210a2e183d7ac562fe0546496ddea8e0dc9
rarepenny <33160365+rarepenny@users.noreply.github.com>
2017-11-03 21:51:29 -0400

    Update plotkey.core.js

--- src/plotkey.core.js
+++ src/plotkey.core.js
@@ -26,6 +26,10 @@ $(document).ready(function () {
 			parse(grabInput());
 		}
 	});
+	
+	$(".gameShortcut").on('click', function(e) {
+		$("#gameCommandInput").val(e.currentTarget);
+	});
 });
 
 var messageDefaults = {
